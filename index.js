a60008f08642ea98032e795ddf7efd1105906b6d
Michael <michael.jonas44@gmail.com>
2015-10-25 22:40:53 -0400

    Testing git and a ssample endpoint

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/item/:item', function(req, res){
+	res.send(req.params.item);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
